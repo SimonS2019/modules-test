13d4e33da754cf5bf425b5b52f5a4b7051776531
SimonS2019 <shentiexiong@gmail.com>
2023-11-21 15:17:28 +1300

    Install Angular Material

--- modules-demo/src/app/app.module.ts
+++ modules-demo/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserAuthModule } from './user-auth/user-auth.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { UserAuthModule } from './user-auth/user-auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserAuthModule
+    UserAuthModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
